ec9a0a926636d93ed541e0ec1ae170ca5ee0e807
jacobvalle <jacobo.valle@g.austincc.edu>
2022-09-12 01:37:50 +0000

    HW 1 w/ test cases

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -7,7 +7,11 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
-
+int multiply_number(int num)
+{
+	auto num1 = 5;
+	return num * num1;
+}
 
 
 
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,27 @@
 ﻿//write include statements
+#include "data_types.h"
+#include <iostream>
 
 
 //write namespace using statement for cout
-
+using std::cin;	using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	int result;
+	result = multiply_numbers(num);
+	cout<<"Result is: "<<result<<"\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"Result is: "<<result<<"\n";
 
 	return 0;
 }
